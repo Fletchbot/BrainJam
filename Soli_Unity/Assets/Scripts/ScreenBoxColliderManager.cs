a0d244b3a227a40ff7900158468ab1df0782200e
Fletchbot <al-fletch@hotmail.co.uk>
2018-08-03 23:55:54 +0100

    delete musecursor, added 3 screen split boxcolliders with script to turn on/off with muse acc when tilted left,centre,right

---
+++ Soli_Unity/Assets/Scripts/ScreenBoxColliderManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UniOSC
+{
+    public class ScreenBoxColliderManager : MonoBehaviour
+    {
+        public GameObject leftBoxCollider, centreBoxCollider, rightBoxCollider;
+
+        public float aY, leftMin, rightMin;
+        private int ratio;
+
+        // Use this for initialization
+        void OnEnable()
+        {
+            ratio = 100;
+            leftMin = 7.5f;
+            rightMin = 10.5f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            aY = UniOSCMuseMonitor.main.accY * ratio;
+
+            if (aY <= leftMin)
+            {
+                leftBoxCollider.GetComponent<ActivateObjects>().SetActive(true);
+
+                centreBoxCollider.GetComponent<ActivateObjects>().SetDeactive(true);
+                rightBoxCollider.GetComponent<ActivateObjects>().SetDeactive(true);
+            }
+            else if (aY >= leftMin && aY <= rightMin)
+            {
+                centreBoxCollider.GetComponent<ActivateObjects>().SetActive(true);
+
+                leftBoxCollider.GetComponent<ActivateObjects>().SetDeactive(true);
+                rightBoxCollider.GetComponent<ActivateObjects>().SetDeactive(true);
+            }
+            else if (aY >= rightMin)
+            {
+                rightBoxCollider.GetComponent<ActivateObjects>().SetActive(true);
+
+                leftBoxCollider.GetComponent<ActivateObjects>().SetDeactive(true);
+                centreBoxCollider.GetComponent<ActivateObjects>().SetDeactive(true);
+            }
+        }
+    }
+}
